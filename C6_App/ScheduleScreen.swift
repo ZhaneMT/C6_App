1e7a3d86d5fe72a4329fdbb16e90c573195a09d1
Zhane Moorman-Taylor <zhaneworkmt@gmail.com>
2024-04-12 20:57:20 -0400

    Here is the overall layout for the app, I created branches so when you push you push to the branch with your name.

---
+++ C6_App/ScheduleScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleScreen.swift
+//  C6_App
+//
+//  Created by Zhane Moorman-Taylor on 4/12/24.
+//
+
+import SwiftUI
+
+struct ScheduleScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScheduleScreen()
+}
