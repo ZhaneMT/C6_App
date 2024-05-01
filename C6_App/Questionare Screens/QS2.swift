58e541f725bc9e64bae9b3350373929add85681f
Zhane Moorman-Taylor <zhaneworkmt@gmail.com>
2024-05-01 13:08:44 -0400

    Here are some new edits and stuff. Specifically the Questionare screens and some minor changes.

---
+++ C6_App/Questionare Screens/QS2.swift
@@ -0,0 +1,18 @@
+//
+//  QS2.swift
+//  C6_App
+//
+//  Created by Zhane Moorman-Taylor on 4/29/24.
+//
+
+import SwiftUI
+
+struct QS2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QS2()
+}
