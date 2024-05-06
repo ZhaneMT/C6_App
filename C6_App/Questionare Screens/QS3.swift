e8a83256a18bed4120a712855751860466dfe7f4
Zhane Moorman-Taylor <zhaneworkmt@gmail.com>
2024-05-06 16:12:12 -0400

    New Developments

--- C6_App/Questionare Screens/QS3.swift
+++ C6_App/Questionare Screens/QS3.swift
@@ -4,18 +4,70 @@
 //
 //  Created by Zhane Moorman-Taylor on 5/2/24.
 //
-
+/*
+func truemon() {
+ 
+        VStack(alignment:.leading){
+            Text("Monday")
+                .bold()
+                .font(.largeTitle)
+                .italic()
+                .foregroundColor(Color(.green))
+                .padding(15)
+            HStack{
+                Text("Time:")
+                    .font(.title)
+                    .bold()
+                    .padding(.trailing, 300.0)
+            }
+        }
+    } else {
+        print("skip")
+    }
+}
+*/
 import SwiftUI
-
+/* @Binding private var Monday: Bool
+ @Binding private var Tuesday: Bool
+ @Binding private var Wednesday: Bool
+ @Binding private var Thursday: Bool
+ @Binding private var Friday: Bool
+ @Binding private var Saturday: Bool
+ @Binding private var Sunday: Bool
+ */
 struct QS3: View {
     var body: some View {
         VStack{
-            Text("What the Fuck ")
-            
+            Text("What hours do you work?")
+                .multilineTextAlignment(.leading)
+                .bold()
+                .font(.largeTitle)
+        VStack(alignment:.leading){
+            Text("Monday")
+                .bold()
+                .font(.largeTitle)
+                .italic()
+                .foregroundColor(Color(.green))
+                .padding(15)
+            HStack{
+                Text("Time:")
+                    .font(.title)
+                    .bold()
+                    .padding(.trailing, 300.0)
+                    ZStack{
+                        Capsule()
+                            .frame(width: 350, height: 10)
+                            .foregroundColor(Color(red: 0.82, green: 0.82, blue: 0.84))
+                        Capsule()
+                            .frame(width: 234, height:10)
+                            .foregroundColor(.green)
+                    }
+                }
+            }
         }
     }
 }
-
-#Preview {
-    QS3()
-}
+        
+       #Preview {
+            QS3()
+        }
--- C6_App/SchedulePage.swift
+++ C6_App/SchedulePage.swift
@@ -6,16 +6,53 @@
 //
 
 import SwiftUI
-
+func printhours(){
+    
+}
 struct SchedulePage: View {
     var body: some View {
-        ZStack{
-            Text("This is the schedule page.")
-            Text("hey")
+        
+        VStack{
+            ZStack{
+                Image("Path")
+                Text("Planner")
+                    .bold()
+                    .font(.largeTitle)
+                HStack{
+                    Button(action: {
+                    }) {
+                        Label("", systemImage: "plus.circle.fill")
+                            .padding(.leading, 380.0)
+                            .foregroundColor(.green)
+                            .font(.largeTitle)
+                    }
+                }
+            }
+            .padding(.trailing, 100.0)
+            .frame(width: 50.0, height: 140.0)
+            .ignoresSafeArea()
+            
+            
+            Spacer()
+            ScrollView{
+                HStack{
+                Text("8:00 AM")
+                        .bold()
+                        .font(.title3)
+                        .italic()
+                    Rectangle()
+                        .frame(width: 300, height: 3)
+        
+                }
+            }
+            
         }
+        
     }
+    
 }
 
+    
 #Preview {
     SchedulePage()
 }
