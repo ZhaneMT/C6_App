9f27382e47b82ab65a5301fa56220bf368942452
Zhane Moorman-Taylor <zhaneworkmt@gmail.com>
2024-05-21 16:22:18 -0400

    Deleted One Scereen and Added Settings Plus timer has now changed into Achievement page

---
+++ C6_App/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  C6_App
+//
+//  Created by Zhane Moorman-Taylor on 5/21/24.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}
