c97508667540a40c7fc67e71d587605a423c541e
Zhane Moorman-Taylor <zhaneworkmt@gmail.com>
2024-05-29 13:36:20 -0400

    This is the new Jar.

---
+++ C6_App/Questionare Screens/SchedulePage.swift
@@ -0,0 +1,18 @@
+//
+//  SchedulePage.swift
+//  C6_App
+//
+//  Created by Zhane Moorman-Taylor on 5/23/24.
+//
+
+import SwiftUI
+
+struct SchedulePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SchedulePage()
+}
